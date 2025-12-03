e740b3fff32f76b3cf1c9ae692f5afbce908636b
PayF123 <141803727+royalarijeet1@users.noreply.github.com>
2025-12-03 18:18:46 +0530

    modified server/index.js

--- server/index.js
+++ server/index.js
@@ -1,6 +1,13 @@
 // server/index.js
 const express = require('express');
+const cors = require('cors');
 const db = require('./database.js');
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
 
 // GET all items
 app.get('/api/items', (req, res) => {
